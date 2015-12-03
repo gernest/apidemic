66f98a8c40f76b0676fda52c39462df05621f6b6
gernest <geofreyernest@live.com>
2015-12-03 08:50:51 +0300

    Add godoc

--- api.go
+++ api.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pmylund/go-cache"
 )
 
+//Version is the version of apidemic. Apidemic uses semver.
 const Version = "0.1"
 
 var maxItemTime = cache.DefaultExpiration
@@ -18,11 +19,13 @@ var store = func() *cache.Cache {
 	return c
 }()
 
+//API is the struct for the json object that is passed to apidemic for registration.
 type API struct {
 	Endpoint string                 `json:"endpoint"`
 	Payload  map[string]interface{} `json:"payload"`
 }
 
+// Home renders hopme page. It renders a json response with information about the service.
 func Home(w http.ResponseWriter, r *http.Request) {
 	details := make(map[string]interface{})
 	details["app_name"] = "ApiDemic"
@@ -32,6 +35,8 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// RenderJSON helder for rndering JSON response, it marshals value into json and writes
+// it into w.
 func RenderJSON(w http.ResponseWriter, code int, value interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	err := json.NewEncoder(w).Encode(value)
@@ -41,6 +46,8 @@ func RenderJSON(w http.ResponseWriter, code int, value interface{}) {
 	}
 }
 
+//RegisterEndpoint receives API objects and registers them. The payload from the request is
+// transformed into a self aware Value that is capable of faking its own attribute.
 func RegisterEndpoint(w http.ResponseWriter, r *http.Request) {
 	a := API{}
 	err := json.NewDecoder(r.Body).Decode(&a)
@@ -62,6 +69,7 @@ func RegisterEndpoint(w http.ResponseWriter, r *http.Request) {
 	RenderJSON(w, http.StatusOK, NewResponse("cool"))
 }
 
+//GetEndpoint renders registered endpoints.
 func GetEndpoint(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	endpoint := vars["endpoint"]
@@ -72,6 +80,7 @@ func GetEndpoint(w http.ResponseWriter, r *http.Request) {
 	RenderJSON(w, http.StatusNotFound, NewResponse("apidemic: "+endpoint+" is not found"))
 }
 
+//NewResponse helper for response JSON message
 func NewResponse(message string) interface{} {
 	return struct {
 		Text string `json:"text"`
@@ -80,6 +89,7 @@ func NewResponse(message string) interface{} {
 	}
 }
 
+//NewServer returns a new apidemic server
 func NewServer() *mux.Router {
 	m := mux.NewRouter()
 	m.HandleFunc("/", Home)
